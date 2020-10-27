82ea68c3d99a1960f2af56e1185836016539999e
aurassai <b-raychev@yandex.com>
2020-10-27 17:17:47 +0200

    Admin User functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,8 +5,12 @@ import {
     getUserProfile,
     registerUser,
     updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserById,
+    updateUser,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.post("/login", authUser);
 router.post("/", registerUser);
@@ -14,4 +18,10 @@ router
     .route("/profile")
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile);
+router.route("/").get(protect, admin, getUsers);
+router
+    .route("/:id")
+    .delete(protect, admin, deleteUser)
+    .get(protect, admin, getUserById)
+    .put(protect, admin, updateUser);
 export default router;
