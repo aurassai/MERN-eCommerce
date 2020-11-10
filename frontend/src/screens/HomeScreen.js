2251fbc03f5dc4f2c46a6dda6a5b23e57335a301
aurassai <b-raychev@yandex.com>
2020-11-10 17:25:29 +0200

    Product Carousel

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,12 +4,13 @@ import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import { listProducts } from "../actions/productActions";
 import Loader from "../components/Loader";
-import Paginate from "../components/Paginate"
+import Paginate from "../components/Paginate";
 import Message from "../components/Message";
+import ProductCarousel from "../components/ProductCarousel";
 
 const HomeScreen = ({ match }) => {
-    const keyword = match.params.keyword
-    const pageNumber = match.params.pageNumber || 1
+    const keyword = match.params.keyword;
+    const pageNumber = match.params.pageNumber || 1;
     const dispatch = useDispatch();
     const productList = useSelector((state) => state.productList);
     const { loading, error, products, pages, page } = productList;
@@ -19,23 +20,28 @@ const HomeScreen = ({ match }) => {
 
     return (
         <>
+            {!keyword && <ProductCarousel />}
             <h1>Latest Products</h1>
             {loading ? (
                 <Loader />
             ) : error ? (
                 <Message variant="danger">{error}</Message>
             ) : (
-                        <>
-                            <Row>
-                                {products.map((product) => (
-                                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                        <Product product={product} />
-                                    </Col>
-                                ))}
-                            </Row>
-                            <Paginate pages={pages} page={page} keyword={keyword ? keyword : ""} />
-                        </>
-                    )}
+                <>
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                    <Paginate
+                        pages={pages}
+                        page={page}
+                        keyword={keyword ? keyword : ""}
+                    />
+                </>
+            )}
         </>
     );
 };
