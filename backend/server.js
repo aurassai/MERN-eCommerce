75a7e7c2a10e60571834517e3f2a2eaae409f449
aurassai <b-raychev@yandex.com>
2020-11-11 16:15:17 +0200

    preparing for hosting and bugfix

--- backend/server.js
+++ backend/server.js
@@ -19,14 +19,10 @@ connectDB();
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
-    app.use(morgan('dev'))
+    app.use(morgan("dev"));
 }
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send("API is running...");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -39,6 +35,18 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    );
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running...");
+    });
+}
 
 // error middleware
 app.use(notFound);
